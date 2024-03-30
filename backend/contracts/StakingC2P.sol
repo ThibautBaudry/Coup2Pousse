71cc13d3f6f039015ef467173c7c7d224e7bf683
ThibautBaudry <thibaut.baudry@gmail.com>
2024-03-30 23:39:19 +0100

    little mistake 2

--- backend/contracts/StakingC2P.sol
+++ backend/contracts/StakingC2P.sol
@@ -8,16 +8,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./C2PToken.sol";
 
-//SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.24;
-
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./C2PToken.sol";
-
 contract Staking is ChainlinkClient {
 
     struct StakingToken {
